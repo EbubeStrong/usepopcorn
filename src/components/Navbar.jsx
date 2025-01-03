05019ae3ef1133f6833634b67af3c64f3b523aac
Abraham Samuel <ebubesammy567@gmail.com>
2025-01-03 17:28:28 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import Search from "./Search"
+
+const Navbar = () => {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+          </div>
+          
+          <Search />
+          
+      <p className="num-results">
+        Found <strong>X</strong> results
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
